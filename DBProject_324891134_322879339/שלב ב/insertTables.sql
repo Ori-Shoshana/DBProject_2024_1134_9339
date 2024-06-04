f2af1980d6844306201c215348d3a3f8f45067de
ori_git <116821177+Ori-Shoshana@users.noreply.github.com>
2024-06-04 22:03:07 +0300

    Delete DBProject_324891134_322879339/שלב ב/insertTables.sql

--- DBProject_324891134_322879339/שלב ב/insertTables.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Arrive (Transportation_id, Student_id) VALUES (10, 372925015);
-INSERT INTO Arrive (Transportation_id, Student_id) VALUES (10, 372925018);
-INSERT INTO Arrive (Transportation_id, Student_id) VALUES (10, 372925026);
-INSERT INTO Arrive (Transportation_id, Student_id) VALUES (10, 372925078);
-INSERT INTO Arrive (Transportation_id, Student_id) VALUES (10, 372925084);
